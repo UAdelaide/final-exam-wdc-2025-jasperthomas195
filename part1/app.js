479a09d7c680e46be1ee7643d1ff305ccfaca20e
Jasper <jasperjthomas@gmail.com>
2025-06-20 04:34:22 +0000

    [automatic commit] 2025-06-20 04:34:22 UTC 58.174.58.12

--- part1/app.js
+++ part1/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const mysql = require('mysql2/promise');
-const fs require('fs');
+const fs = require('fs');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+let db;
+
