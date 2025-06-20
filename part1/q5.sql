b054bc2c6a5f7e27f8aec2cc780370f4618e1734
Jasper <jasperjthomas@gmail.com>
2025-06-20 03:49:05 +0000

    [automatic commit] 2025-06-20 03:49:05 UTC 58.174.58.12

--- part1/q5.sql
+++ part1/q5.sql
@@ -4,11 +4,12 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('lebronjames', 'lebron@example.com', 'hashed321', 'walker'),
-('jasper', 'jasper@example.com', 'hashed987', 'owner'),
+('jasper', 'jasper@example.com', 'hashed987', 'owner');
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'lebronjames'), 'Sam', 'large'),
 ((SELECT user_id FROM Users WHERE username = 'jasper'), 'Spot', 'medium'),
-((SELECT user_id FROM Users WHERE username = 'jasper'), 'Spot', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'jasper'), 'Reggie', 'large');
+
