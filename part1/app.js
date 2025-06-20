6fb1e46b964fbac35e14e734c0697666c808bdbb
Jasper <jasperjthomas@gmail.com>
2025-06-20 08:32:35 +0000

    [automatic commit] 2025-06-20 08:32:35 UTC 58.174.58.12

--- part1/app.js
+++ part1/app.js
@@ -42,7 +42,6 @@ let db;
 
 async function insertData() {
     try {
-
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
@@ -50,7 +49,8 @@ async function insertData() {
         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
         ('lebronjames', 'lebron@example.com', 'hashed321', 'walker'),
         ('jasper', 'jasper@example.com', 'hashed987', 'owner')
-
+      `);
+      
 
     }
 }
