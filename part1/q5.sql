696d39323d330d543dbe92e87a9c570a0ce0aad8
Jasper <jasperjthomas@gmail.com>
2025-06-20 03:51:52 +0000

    [automatic commit] 2025-06-20 03:51:52 UTC 58.174.58.12

--- part1/q5.sql
+++ part1/q5.sql
@@ -14,4 +14,4 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'jasper'), 'Reggie', 'large');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-(SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (select user_id FROM Users WHERE username = 'alice123')), ''
+(SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (select user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands'
