fc5c0a43339cbc67e9c02a72c25b0b884444aabb
Jasper <jasperjthomas@gmail.com>
2025-06-20 03:57:30 +0000

    [automatic commit] 2025-06-20 03:57:30 UTC 58.174.58.12

--- part1/q5.sql
+++ part1/q5.sql
@@ -2,7 +2,6 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('lebronjames', 'lebron@example.com', 'hashed321', 'walker'),
 ('jasper', 'jasper@example.com', 'hashed987', 'owner');
 
