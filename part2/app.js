21fc22ea7e2eee66fd80f6be638770151d577e62
Jasper <jasperjthomas@gmail.com>
2025-06-20 14:21:20 +0000

    [automatic commit] 2025-06-20 14:21:19 UTC 58.174.58.12

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session'); // this was added too
+const session = require('express-session'); // added to allow below to work
 require('dotenv').config();
 
 const app = express();
