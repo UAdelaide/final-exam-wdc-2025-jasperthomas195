4f54e54a692aee9932598e854d92ab9f6d76f6e3
Jasper <jasperjthomas@gmail.com>
2025-06-20 03:46:35 +0000

    [automatic commit] 2025-06-20 03:46:35 UTC 58.174.58.12

--- part1/q5.sql
+++ part1/q5.sql
@@ -8,3 +8,4 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
