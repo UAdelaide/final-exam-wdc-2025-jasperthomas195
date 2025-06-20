e40e87650af0ebd7d72fb8308accec4800d809e2
Jasper <jasperjthomas@gmail.com>
2025-06-20 13:27:53 +0000

    [automatic commit] 2025-06-20 13:27:53 UTC 58.174.58.12

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,27 +37,19 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE username = ? AND password_hash = ?
-    `, [username, password]);
+      WHERE email = ? AND password_hash = ?
+    `, [email, password]);
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    req.session.user = {
-      id: rows[0].user_id,
-      username: rows[0].username,
-      role: rows[0].role
-    };
-
-    res.json({ message: 'Login successful',
-    role: rows[0].role
-    });
+    res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
