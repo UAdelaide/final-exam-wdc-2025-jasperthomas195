d6c5e2e7b5befc9f9613542ba28715634795100a
Jasper <jasperjthomas@gmail.com>
2025-06-20 09:40:29 +0000

    [automatic commit] 2025-06-20 09:40:29 UTC 58.174.58.12

--- part1/server.js
+++ part1/server.js
@@ -2,4 +2,5 @@ const app = require('./app');
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}` ")
+    console.log(`Server running on port: ${port}`);
+});
